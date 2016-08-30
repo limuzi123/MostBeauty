27a2b647bdb1c8726d840e3d886b920b7ab489fe
smillingSoul <2286071059@qq.com>
2016-08-30 17:47:07 +0800

    建adapter包

--- MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/fragment/HaveThingsFragment.java
+++ MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/fragment/HaveThingsFragment.java
@@ -1,5 +1,8 @@
 package com.lanou3g.mostbeauty.fragment;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+
 import com.lanou3g.mostbeauty.R;
 import com.lanou3g.mostbeauty.base.BaseFragment;
 
@@ -7,6 +10,8 @@ import com.lanou3g.mostbeauty.base.BaseFragment;
  * Created by dllo on 16/8/30.
  */
 public class HaveThingsFragment extends BaseFragment{
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Override
     protected int initLayout() {
         return R.layout.fragment_havethings;
@@ -14,7 +19,8 @@ public class HaveThingsFragment extends BaseFragment{
 
     @Override
     protected void initView() {
-
+        tabLayout= (TabLayout) getActivity().findViewById(R.id.haveThings_tl);
+        viewPager= (ViewPager) getActivity().findViewById(R.id.haveThings_vp);
     }
 
     @Override
