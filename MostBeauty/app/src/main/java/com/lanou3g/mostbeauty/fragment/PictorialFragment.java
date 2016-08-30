51bf1fe2a412435a3f6fe42dab0903314caef99c
limuzi123 <1355413352@qq.com>
2016-08-30 19:44:08 +0800

    修改Fragment

--- MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/fragment/PictorialFragment.java
+++ MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/fragment/PictorialFragment.java
@@ -8,14 +8,14 @@ import com.lanou3g.mostbeauty.base.BaseFragment;
 /**
  * Created by dllo on 16/8/30.
  */
-public class PictorialFragment extends BaseFragment{
+public class PictorialFragment extends BaseFragment {
     @Override
     protected int initLayout() {
         return R.layout.fragment_pictorial;
     }
 
     @Override
-    protected void initView(View view) {
+    protected void initView() {
 
     }
 
