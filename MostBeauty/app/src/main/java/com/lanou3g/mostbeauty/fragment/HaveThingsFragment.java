78e6bec7141deeb5a6a5bb09bbc4d7dd684a0fdf
limuzi123 <1355413352@qq.com>
2016-08-30 15:14:15 +0800

    四个Fragment

---
+++ MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/fragment/HaveThingsFragment.java
@@ -0,0 +1,24 @@
+package com.lanou3g.mostbeauty.fragment;
+
+import com.lanou3g.mostbeauty.R;
+import com.lanou3g.mostbeauty.base.BaseFragment;
+
+/**
+ * Created by dllo on 16/8/30.
+ */
+public class HaveThingsFragment extends BaseFragment{
+    @Override
+    protected int initLayout() {
+        return R.layout.fragment_havethings;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
