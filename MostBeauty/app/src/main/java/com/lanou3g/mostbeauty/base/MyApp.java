58e792d9d982585f758486bc2d7000f7dd94304d
limuzi123 <1355413352@qq.com>
2016-08-30 14:29:43 +0800

    base包

---
+++ MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/base/MyApp.java
@@ -0,0 +1,16 @@
+package com.lanou3g.mostbeauty.base;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by dllo on 16/8/30.
+ */
+public class MyApp extends Application{
+    protected static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
