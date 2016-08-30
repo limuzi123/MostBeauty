f6cf6337218d49f8c308b859cf976191d8dcf347
limuzi123 <1355413352@qq.com>
2016-08-30 15:05:06 +0800

    activity

--- MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/activity/MainActivity.java
+++ MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/activity/MainActivity.java
@@ -1,8 +1,9 @@
-package com.lanou3g.mostbeauty;
+package com.lanou3g.mostbeauty.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.lanou3g.mostbeauty.R;
 import com.lanou3g.mostbeauty.base.BaseActivity;
 
 public class MainActivity extends BaseActivity{
